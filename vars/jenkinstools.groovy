5516ec3b0f0eb97457209575628a3945e24340de
pankaj <panvic99@gmail.com>
2025-04-17 13:49:14 +0530

    shared lib jenkins file created vars folder

--- vars/jenkinstools.groovy
+++ vars/jenkinstools.groovy
@@ -3,9 +3,8 @@ def call(Map s){
         sh 'ls -ltr'
         sh 'pwd'
         sh 'ls -ltr'
-        pa = s.paths
-        echo 'path in lib : $pa'
-        dir('Jenkins/maven-samples/single-module'){
+        echo 'path in lib : $s.paths'
+        dir('s.paths'){
             sh 'mvn package' 
         }
         
