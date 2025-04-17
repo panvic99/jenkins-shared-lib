1abf43e5c45a2a5d9242abefae0638c7801f0413
pankaj <panvic99@gmail.com>
2025-04-17 13:24:56 +0530

    shared lib jenkins file created vars folder

--- vars/jenkinstools.groovy
+++ vars/jenkinstools.groovy
@@ -2,9 +2,10 @@ def call(){
     node{
         sh 'ls -ltr'
         dir('Jenkins/maven-samples/single-module'){
+            sh 'ls -ltr'
            sh 'mvn package' 
            //sh 'false'
-           sh 'ls -ltr'
+           
         }
     }
 }
